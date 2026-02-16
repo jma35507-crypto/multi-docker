131cb80640f24c40ec8a8f5e29c6ad0237a25df0
jasonmatthewsdatadoghq <jason.matthews@datadoghq.com>
2026-02-17 08:30:03 +1100

    Update 4

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// update the text in some way. If you want to start measuring performance in your app, pass a function
+// update 3. If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
